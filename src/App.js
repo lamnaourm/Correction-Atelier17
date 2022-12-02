620fdf09cd89b886bba05b2de0ab872d9bef0b0c
lamnaourm <mohammed.lamnaour@gmail.com>
2022-12-02 10:13:54 +0100

    affiche les universites

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import DetailsCountry from './components/DetailsCountry';
 import ListCountry from './components/ListCountry';
+import ListUniversity from './components/ListUniversity';
 
 function App() {
   return (
     <div>
       <ListCountry />
       <DetailsCountry />
+      <ListUniversity />
     </div>
   );
 }
--- src/components/ListUniversity.jsx
+++ src/components/ListUniversity.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 export default function ListUniversity() {
+
+  const Selectedcountry = useSelector(state => state.country.selectedCountry);
+  const listUniversities = useSelector(state => state.university.data).filter(item => item.country===Selectedcountry);
+
   return (
-    <div>ListUniversity</div>
+    <div>
+
+        <h1>Liste des universites</h1>
+        {listUniversities.length!==0 && 
+            listUniversities.map(item => <h2>{item.name}</h2>)
+        }
+    </div>
   )
 }
